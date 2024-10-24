4825e8650b04544aa15d4fdc7a0969c4a98912d7
aiktb <ai.ourship@gmail.com>
2024-10-24 20:53:42 +0800

    feat: binding `URLSearchParams` with the editor value

---
+++ app/assets/example.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <span class="count">{{ count }}</span>
+  <button @click="count++">Add</button>
+</template>
+
+<style scoped>
+.count {
+  color: green;
+}
+</style>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,14 @@
-import type { MetaFunction } from "@netlify/remix-runtime";
+import type { LoaderFunctionArgs, MetaFunction } from "@netlify/remix-runtime";
+import { json } from "@netlify/remix-runtime";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import { parse } from "@vue/compiler-sfc";
 
 import { ClientOnly } from "remix-utils/client-only";
 import AstJsonPreview from "~/components/ast-json-preview";
 import CodeMirrorEditor from "~/components/code-mirror-editor";
 import Header from "~/components/header";
+
+import exampleCode from "~/assets/example.vue?raw";
 
 export const meta: MetaFunction = () => {
   return [
@@ -16,28 +20,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-const exampleCode = `<script setup lang="ts">
-import { ref } from 'vue';
-
-const count = ref(0);
-</script>
-
-<template>
-  <span class="count">{{ count }}</span>
-  <button @click="count++">Add</button>
-</template>
-
-<style scoped>
-.count {
-  color: green;
-}
-</style>
-`;
+export const loader = ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("code");
+  if (q) {
+    return json({ code: q });
+  }
+  return json({ code: exampleCode });
+};
 
 export default function Index() {
-  const [code, setCode] = useState(exampleCode);
+  const { code: defaultCode } = useLoaderData<typeof loader>();
+  const [code, setCode] = useState(defaultCode);
   const ast = parse(code);
 
+  const [_, setSearchParams] = useSearchParams();
+  const onChange = (newCode: string) => {
+    if (newCode === code) {
+      return;
+    }
+    setCode(newCode);
+    const newParams = new URLSearchParams();
+    newParams.set("code", newCode);
+    setSearchParams(newParams);
+  };
   return (
     <div className="relative grid h-dvh grid-rows-[3rem,_1fr]">
       <Header className="sticky top-0 z-20 w-full" />
@@ -45,7 +51,7 @@ export default function Index() {
         <ResizablePanelGroup direction="horizontal" className="w-full overflow-scroll">
           <ResizablePanel defaultSize={50}>
             <div className="h-full items-center justify-center p-6">
-              <ClientOnly>{() => <CodeMirrorEditor code={code} onChange={setCode} />}</ClientOnly>
+              <ClientOnly>{() => <CodeMirrorEditor code={code} onChange={onChange} />}</ClientOnly>
             </div>
           </ResizablePanel>
           <ResizableHandle withHandle={true} />
